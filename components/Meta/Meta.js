75753808eb26470f83f798456c826d8f8c18efad
Aditya Magar <magaraditya2911@gmail.com>
2026-02-13 13:19:11 +0530

    logo and metadata changed

--- components/Meta/Meta.js
+++ components/Meta/Meta.js
@@ -31,23 +31,8 @@ const Meta = () => (
     <meta property="twitter:url" content={METADATA.siteUrl} />
     <meta property="twitter:image" content={METADATA.image} />
 
-    <link
-      rel="apple-touch-icon"
-      sizes="180x180"
-      href="/favicons/apple-touch-icon.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="32x32"
-      href="/favicons/favicon-32x32.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="16x16"
-      href="/favicons/favicon-16x16.png"
-    />
+    <link rel="icon" type="image/png" href="/logo.png" />
+    <link rel="apple-touch-icon" href="/logo.png" />
     <meta name="msapplication-TileColor" content="#7000FF" />
     <link rel="manifest" href="/manifest.json" />
   </Head>
